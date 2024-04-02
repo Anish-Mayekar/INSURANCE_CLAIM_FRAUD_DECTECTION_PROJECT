d219d02253d98d93739ca3e307ab57844e06b64a
Soham AP <113892188+SohamAP22@users.noreply.github.com>
2024-04-02 19:51:00 +0530

    Add files via upload

---
+++ src/routes/AllReports.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/AdminSidebarStyles.css";
+import AdminSidebar from "../components/AdminSidebar";
+
+function Reports() {
+  return (
+    <>
+      <AdminSidebar />
+      <div className="reports">
+        <h1>Reports</h1>
+      </div>
+    </>
+  );
+}
+
+export default Reports;
